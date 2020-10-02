8b7559454a91cae06d70f1e5aea7df1ba767fa73
ks6088ts <ks6088ts@gmail.com>
2020-10-02 14:41:53 +0900

    add input box

--- src/extension.ts
+++ src/extension.ts
@@ -4,14 +4,21 @@ import {getDateFormat} from './dateformat';
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('sandbox-vscode-extension.sendMail', () => {
-			let sender = vscode.workspace.getConfiguration("sandbox").sender;
-			let receiver = vscode.workspace.getConfiguration("sandbox").receiver;
-			let subject = "[" + sender + "]";
-			let body = "hello world @ " + getDateFormat(new Date());
-			vscode.window.showInformationMessage(sender);
-			vscode.env.openExternal(
-				vscode.Uri.parse("mailto:" + receiver + "?subject=" + subject + "&body=" + body)
-			);
+			vscode.window.showInputBox({
+				prompt: "Type messages",
+				validateInput: (s:string): string | undefined => (!s) ? "You must input something." : undefined
+			}).then(
+				inputString => {
+					// vscode.window.showInformationMessage(`input: ${inputString}`);
+					let sender = vscode.workspace.getConfiguration("sandbox").sender;
+					let receiver = vscode.workspace.getConfiguration("sandbox").receiver;
+					let subject = "[" + sender + "]";
+					let body = `${inputString} @ ${getDateFormat(new Date())}`;
+					vscode.window.showInformationMessage(sender);
+					vscode.env.openExternal(
+						vscode.Uri.parse("mailto:" + receiver + "?subject=" + subject + "&body=" + body)
+					);
+				});
 		})
 	);
 }
